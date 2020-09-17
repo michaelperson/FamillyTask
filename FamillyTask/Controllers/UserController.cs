e3ecbaace067b55a467fa14376212e28c1b038ec
mperson <michael.person@cognitic.be>
2020-09-17 14:47:03 +0200

    Create The CreateToken function

--- FamillyTask/Controllers/UserController.cs
+++ FamillyTask/Controllers/UserController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using FamillyTask.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FamillyTask.Controllers
 {
@@ -38,6 +41,20 @@ namespace FamillyTask.Controllers
             }
 
             return result;
+        }
+
+
+        private string CreateToken(LoginModel model)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this._configuration["jwt:key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(this._configuration["jwt:issuer"],
+                                             this._configuration["jwt:issuer"],
+                                             expires: DateTime.Now.AddMinutes(30),
+                                             signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
