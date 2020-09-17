d652cdcc7a61283e5bc72260856f39e8aed05f56
mperson <michael.person@cognitic.be>
2020-09-17 16:03:39 +0200

    Add USersRepository and A new function GetByLoginPass

---
+++ FamillyTask.DAL/Repositories/UsersReposiotry.cs
@@ -0,0 +1,41 @@
+﻿using FamillyTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FamillyTask.DAL.Repositories
+{
+    public class UsersReposiotry : BaseRepository<Users, int>
+    {
+        public override bool Add(Users obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Delete(Users id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Users> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Users GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public  Users GetByLoginPass(Users partialUser)
+        {
+            return base.ExecuteReaderWithParameters("Select * from Users Where Login=@Login and Passwd=@Passwd",System.Data.CommandType.Text, partialUser).FirstOrDefault();
+        }
+
+        public override bool Update(Users obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
