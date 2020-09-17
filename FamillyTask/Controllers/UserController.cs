5014d8d6a8d9d45ea12a1f4100cdbb74202a4278
mperson <michael.person@cognitic.be>
2020-09-17 14:46:03 +0200

    Create Login Endpoint With Anonymous access Create LoginModel and UserModel

--- FamillyTask/Controllers/UserController.cs
+++ FamillyTask/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FamillyTask.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -19,6 +20,24 @@ namespace FamillyTask.Controllers
         public UserController(IConfiguration configuration)
         {
             _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginModel model)
+        {
+            IActionResult result = this.Unauthorized();
+
+            if (model != null)
+            {
+                UserModel resultModel = new UserModel();
+
+                resultModel.Token = this.CreateToken(model);
+
+                result = this.Ok(resultModel);
+            }
+
+            return result;
         }
     }
 }
---
+++ FamillyTask/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamillyTask.Models
+{
+    public class LoginModel
+    {
+        public string Login { get; set; }
+        public string Passwd { get; set; }
+    }
+}
