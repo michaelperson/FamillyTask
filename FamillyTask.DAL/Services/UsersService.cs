3dbff4bdd8d98ddd2ae96c88173efc792fc2758d
mperson <michael.person@cognitic.be>
2020-09-17 16:07:51 +0200

    Add Service for Users and function to CheckLogin

--- FamillyTask.DAL/Services/UsersService.cs
+++ FamillyTask.DAL/Services/UsersService.cs
@@ -1,22 +1,28 @@
-﻿using FamillyTask.Models;
+﻿using FamillyTask.DAL.Repositories;
+using FamillyTask.Models;
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
 using System.Text;
 
-namespace FamillyTask.DAL.Repositories
+namespace FamillyTask.DAL.Services
 {
-    public class UsersReposiotry : BaseRepository<Users, int>
+    public class UsersService : BaseService<Users , int>
     {
-        public UsersReposiotry(IDbConnection connection) : base(connection)
+        UsersReposiotry repo;
+        public UsersService(string ConnectionString) : base(ConnectionString)
         {
-
+            repo = new UsersReposiotry(connection);
         }
 
-        public override bool Add(Users obj)
+        public override bool Add(Users id)
         {
             throw new NotImplementedException();
+        }
+
+        public Users CheckLogin(string Login, String Passwd)
+        {
+            Users users = new Users() { Login = Login, Password = Passwd };
+            return repo.GetByLoginPass(users);
         }
 
         public override bool Delete(Users id)
@@ -32,11 +38,6 @@ namespace FamillyTask.DAL.Repositories
         public override Users GetOne(int id)
         {
             throw new NotImplementedException();
-        }
-
-        public  Users GetByLoginPass(Users partialUser)
-        {
-            return base.ExecuteReaderWithParameters("Select * from Users Where Login=@Login and Password=@Passwd", System.Data.CommandType.Text, partialUser).FirstOrDefault();
         }
 
         public override bool Update(Users obj)
